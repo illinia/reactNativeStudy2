14dad57f802c617ee31aa2019ef86661b2384573
illinia <67290425+illinia@users.noreply.github.com>
2021-06-28 21:05:23 +0300

    todo app

---
+++ src/components/Input.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+
+const StyledInput = styled.TextInput.attrs(({ theme }) => ({
+  placeholderTextColor: theme.main,
+}))`
+  width: ${({ width }) => width - 40}px;
+  height: 60px;
+  margin: 3px 0;
+  padding: 15px 20px;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.itemBackground};
+  font-size: 25px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Input = ({ placeholder }) => {
+  const width = Dimensions.get('window').width;
+  return (
+    <StyledInput
+      width={width}
+      placeholder={placeholder}
+      maxLength={50}
+      autoCapitalize="none"
+      autoCorrect={false}
+      returnKeyType="done"
+      keyboardAppearance="dark"
+    />
+  );
+};
+
+export default Input;
