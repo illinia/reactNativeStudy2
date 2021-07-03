7d98e5356a8d1781028a762880041317b5b6ebdb
illinia <67290425+illinia@users.noreply.github.com>
2021-07-03 22:34:52 +0300

    302~365

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+const colors = {
+  white: '#fff',
+  black: '#000',
+  grey_0: '#d5d5d5',
+  grey_1: '#a6a6a6',
+  red: '#e84118',
+  blue: '#3679fe',
+};
+
+export const theme = {
+  background: colors.white,
+  text: colors.black,
+  imageBackground: colors.grey_0,
+  imageButtonBackground: colors.grey_1,
+  imageButtonIcon: colors.white,
+  label: colors.grey_1,
+  inputPlaceholder: colors.grey_1,
+  inputBorder: colors.grey_1,
+  errorText: colors.red,
+  buttonBackground: colors.blue,
+  buttonTitle: colors.white,
+  buttonUnfilledTitle: colors.blue,
+  headerTintColor: colors.black,
+};
