89610315ef1d3860c25f51dab8d90edff2861125
illinia <67290425+illinia@users.noreply.github.com>
2021-07-09 19:15:36 +0300

    re-react-native-component

---
+++ src/components/MyButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Pressable, Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+const MyButton = props => {
+  return (
+    <Pressable
+      style={{
+        backgroundColor: '#3498db',
+        padding: 16,
+        margin: 10,
+        borderRadius: 8,
+      }}
+      onPress={() => props.onPress()}
+    >
+      <Text style={{ color: 'white', fontSize: 24 }}>
+        {props.children || props.title}
+      </Text>
+    </Pressable>
+  );
+};
+
+MyButton.defaultProps = {
+  title: 'Default'
+};
+
+MyButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired,
+};
+
+export default MyButton;
